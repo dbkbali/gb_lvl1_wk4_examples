cbc379bf29e63013a1b9a6fc755f04f873a35e5c
David Krett <dbkrett@gmail.com>
2018-10-05 18:23:27 -0400

    Initial Commit

---
+++ Readme.md
@@ -0,0 +1,4 @@
+# Javascript Examples
+
+Some examples to highlight how Javascript processes instruction using a synchronous 
+Event Loop
---
+++ ex_1.js
@@ -0,0 +1,17 @@
+function sayHi() {
+  console.log("Hi There !!")
+}
+
+function sayBye() {
+  console.log("By There")
+}
+
+
+function sayGottaGo() {
+  console.log("Got To Go now")
+}
+
+
+sayHi();
+sayBye();
+sayGottaGo();
